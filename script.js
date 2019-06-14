7f0081b3368ece9aadcd7481c5b08634bbbb6ab2
Thomas Kirkland <thomasckirkland@gmail.com>
2019-06-14 13:49:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log("Good Morning!");
+// alert("Hello!")
+// DOM Elements
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  focus = document.getElementById('focus');
+
+// Show Time
+
+function showTime(){
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+    //Set AM or PM
+
+const amPM = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+
+hour = hour % 12 || 12;
+
+    //Output Time
+
+time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+
+setTimeout(showTime, 1000);
+
+}
+showTime();
+
+
+
+// Run
+
+// showTime();
